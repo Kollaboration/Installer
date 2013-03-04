9aaa04625f0785531bee888ebea6a1aa988539fe
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-04 19:02:49 +0100

    makes package install script more verbose

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# tries to install a given package with the package
-# script name provided
 
 function fail() {
     echo $1 # show error
@@ -20,13 +18,7 @@ fi
 
 # check if package is already installed
 if [ -f "$HOME/.kpkg/packages/installed" ]; then
-    fail "package $1 already installed"
+    echo "package $1 is already installed"
+else
+    fail "package $1 is not installed yet"
 fi
-
-# every possible error has been handled
-# we can pass control to the installation
-# script of the package
-echo "installing $1..."
-script="$HOME/.kpkg/packages/$1/install.sh"
-shift # remove first command line argument
-source $script $@ # call script with parameters
