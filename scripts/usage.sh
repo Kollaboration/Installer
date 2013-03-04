09f70d46f3cbfcedad315a40987ccf110bd40d6c
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-04 21:26:40 +0100

    finally adds the usage.sh script

---
+++ scripts/usage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# show usage
+
+echo "Usage: kpkg <command> <package-name>"
+echo "available commands are:"
+echo " install  Installs the given package"
+echo " check    Checks if the given package is installed"
+echo " list     Lists available and installed packages"
+echo " require  Installs a given package if it's not installed yet"
+echo " cleanup  Cleans up temporary files"
