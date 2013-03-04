ac0e81d4d1d3ca5c6f9829a14b13acb94d309b22
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-04 19:28:48 +0100

    fixes kpkg

--- kpkg.sh
+++ kpkg.sh
@@ -2,13 +2,25 @@
 # this script checks if it is correctly installed,
 # and if it is, passes the work on to other scripts
 
+# see if any command was passed, and if not, call usage
+if [ -z "$1" ]; then
+    if [ -f "$HOME/.kpkg/scripts/usage.sh" ]; then
+        source "$HOME/.kpkg/scripts/usage.sh"
+    else
+        echo "unrecognized command"
+    fi
+
+    exit -1
+fi
+
 # determine if it's been installed
 if [ ! -d "$HOME/.kpkg/scripts" ]; then
     echo "not installed! run install.sh first."
     exit -1
 fi
 
-if [ -d "$HOME/.kpkg/scripts/$1.sh" ]; then
+# see if script exists and run it
+if [ -f "$HOME/.kpkg/scripts/$1.sh" ]; then
     # save path to script
     script="$HOME/.kpkg/scripts/$1.sh"
 
@@ -22,7 +34,7 @@ else
     echo "unrecognized command \"$1\""
     
     # show usage (if there is such a script)
-    if [ -d "$HOME/.kpkg/scrips/usage.sh" ]; then
+    if [ -f "$HOME/.kpkg/scrips/usage.sh" ]; then
         source "$HOME/.kpkg/scripts/usage.sh"
     fi
 
